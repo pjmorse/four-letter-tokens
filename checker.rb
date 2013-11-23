36a37758abef50684718b4c344f82ed1cf4581d9
Parker Morse <pmorse@concord.org>
2013-11-23 16:53:53 -0500

    This seems to work, but it's slow.

--- checker.rb
+++ checker.rb
@@ -1,8 +1,16 @@
 class Checker
+  require './word.rb'
+
   # Gets a dictionary file as input
   def initialize(filename)
     @infile = filename
+    @seq_count = 0
     check_file
+  end
+
+  def sequence_file_check
+    @sequences = @sequences || File.open('sequences', 'r')
+    @sequences.rewind
   end
 
   def check_file
@@ -12,33 +20,35 @@ class Checker
     # Open sequences file for writing
     sequences = File.open('sequences', 'w')
 
-    # Open sequences file for checking
-    # Same file, different filehandle
-    sequence_check = File.open('sequences', 'r')
-
     # Open words file
     words = File.open('words', 'w')
 
     # Read dictionary
     word_list.each do |word|
-      w = Word.new(word)
+      w = Word.new(word.chop)
       w.get_tokens.each do |seq|
-        # For each word, check sequences against sequences file
-        sequence_check.rewind
-        unique = false
-        sequence_check.each do |known_seq|
-          if known_seq == seq
-            break
-          end
-          unique = true
-        end
-        if unique
+        if check_sequence(seq)
           # If not found, write to sequences file
           # And write word to words file
+          @seq_count = @seq_count + 1
           sequences.write("#{seq}\n")
           words.write("#{word}\n")
         end
       end
     end
+    return @seq_count
+  end
+
+  def check_sequence(test_seq)
+    # For each word, check sequences against sequences file
+    unique = true
+    sequence_file_check
+    @sequences.each do |known_seq|
+      if known_seq == test_seq
+        unique = false
+        break
+      end
+    end
+    return unique
   end
 end
