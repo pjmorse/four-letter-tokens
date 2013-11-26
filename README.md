254af76aed62f64fe13763eb07414f755cb143d8
Parker Morse <flashesofpanic@gmail.com>
2013-11-25 21:06:18 -0500

    Remove now-unused code.

--- README.md
+++ README.md
@@ -3,4 +3,11 @@ Four Letter Tokens
 
 A Ruby solution for https://gist.github.com/seanthehead/7220609 .
 
-To run, after cloning this repository, open IRB, require `'./checker.rb'`, and instantiate `Checker` with the relative path to the dictionary file as an argument (e.g. `check = Checker.new('./dictionary.txt')`). Result will be written to `sequences` and `words`.
+To run, after cloning this repository, run `./find_tokens.rb` and supply the name of a dictionary file. `dictionary.txt` is included in the repository, so `./find_tokens.rb ./dictionary.txt` from the project directory will run on that file.
+
+The command will return a message reporting how many unique four-letter sequences were found in that file.
+
+Self-checking
+--------
+
+The `FourLetterTokens::Word` class will self-test in IRB; run `FourLetterTokens::Word.test` to validate that the class is reporting the tokens from different words properly. The `FourLetterTokens::Checker` class does not have this feature.
--- checker.rb
+++ checker.rb
@@ -7,44 +7,23 @@ module FourLetterTokens
       @infile = filename
       @seq_count = 0
       @found_sequences = {}
+      # N.B. the actual work gets done here where `check_file` is called.
       puts "#{check_file} sequences found."
       write_results
     end
 
-    # Verify that there is a sequence file open for checking, with the file
-    # read at the beginning
-    def sequence_file_check
-      @sequences = @sequences || File.open('sequences', 'r')
-      @sequences.rewind
-    end
-
-    # Open dictionary
-    def open_word_list
-      @word_list = File.open(@infile)
-    end
-
-    # Open sequences file for writing
-    def open_sequence_file
-      @sequences = File.open('sequences', 'w')
-    end
-
-    # Open words file
-    def open_words_file
-      @words = File.open('words', 'w')
-    end
-
     # Checks the provided word list for unique sequences, populating the list and returning a count.
     def check_file
-      open_word_list
-
       # Read dictionary
+      @word_list = File.open(@infile)
       @word_list.each do |word|
         w = Word.new(word.chop)
+        # Check each token from word
         w.get_tokens.each do |seq|
-          if check_sequence(seq)
-            # If not found, add to sequences array
-            @seq_count = @seq_count + 1
+          if check_sequence(seq) # Returns true if unique so far
+            # Add to sequences array
             @found_sequences[seq] = word.chop
+            @seq_count = @seq_count + 1
           end
         end
       end
@@ -58,16 +37,15 @@ module FourLetterTokens
 
     # Output @found_sequences array to files
     def write_results
-      unless @found_sequences.empty?
-        open_sequence_file
-        open_words_file
-        @found_sequences.each_pair do |sequence, word|
-          @sequences.write "#{sequence}\n"
-          @words.write "#{word}\n"
-        end
-        @sequences.close
-        @words.close
+      # Open files for writing
+      sequences_file = File.open('sequences', 'w')
+      words_file = File.open('words', 'w')
+      @found_sequences.each_pair do |sequence, word|
+        sequences_file.write "#{sequence}\n"
+        words_file.write "#{word}\n"
       end
+      sequences_file.close
+      words_file.close
     end
   end
 end
